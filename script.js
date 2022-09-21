594b464ea4fcc73f10d00fb88b17e49ea11f2ed8
Rince Mathew <rincemathewmanattu@gmail.com>
2022-09-21 22:27:21 +0530

    using objects

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // console.log()
 // var blue1,blue2,blue3,green1,green2,green3;
+// var color = [blue:{one:'', two:''}]
 var colorBlue = {blue1: '', blue2: '', blue3: ''};
-var colorGreen = {green1: '', green2: '', green3: ''}
-var countColor = 1;
+var colorGreen = {green1: '', green2: '', green3: ''};
+// var countColor = 1;
 var player = true;
 
-
-
+// console.log(colorBlue)
+console.log(Object.values(colorGreen))
 //What happens when click the button
 function buttonEvent(id) {
-
-    if (countColor < 7) {
+    
+    if (colorGreen.green3 == '') {
         firstEvent(id);
+    } else {
+        console.log('dsdsd')
     }
     
 }
@@ -20,13 +23,37 @@ function buttonEvent(id) {
 function firstEvent(id) {
     if(player){
         document.getElementById(id).style.backgroundColor="#0066ff";
-        countColor++;
+        // countColor++;
         player = !player
+        for (var i=0; i < Object.values(colorBlue).length; i++) {
+            if (Object.values(colorBlue)[i] == '') {
+                // colorBlue.Object.keys
+                var j = Object.keys(colorBlue)[i]
+                colorBlue[j] = id
+                console.log(j)
+                break;
+            }
+        }
+        gameOver();
     } else {
         document.getElementById(id).style.backgroundColor="#00cc00";
-        countColor++;
+        // countColor++;
         player = !player
+        for (var i=0; i < Object.values(colorGreen).length; i++) {
+            if (Object.values(colorGreen)[i] == '') {
+                var j = Object.keys(colorGreen)[i]
+                colorGreen[j] = id
+                console.log(j)
+                break;
+            }
+        }
+        gameOver();
     }
+}
+
+//check the game is finished or not
+function gameOver() {
+    console.log(colorBlue)
 }
 
 
