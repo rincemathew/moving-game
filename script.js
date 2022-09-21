dab717353958bfc0023ea48998d958617e5a06b5
Rince Mathew <rincemathewmanattu@gmail.com>
2022-09-21 22:58:24 +0530

    changed it to array

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 // console.log()
-// var blue1,blue2,blue3,green1,green2,green3;
-// var color = [blue:{one:'', two:''}]
-var colorBlue = {blue1: '', blue2: '', blue3: ''};
-var colorGreen = {green1: '', green2: '', green3: ''};
+var colorBlue = [];
+var colorGreen = [];
+var colorList = []
 // var countColor = 1;
 var player = true;
 
-// console.log(colorBlue)
-console.log(Object.values(colorGreen))
 //What happens when click the button
 function buttonEvent(id) {
     
-    if (colorGreen.green3 == '') {
-        firstEvent(id);
+    if (colorGreen.length < 3) {
+        return colorList.includes(id) ? alert('you already clicked this'): firstEvent(id);
     } else {
         console.log('dsdsd')
     }
@@ -23,37 +20,25 @@ function buttonEvent(id) {
 function firstEvent(id) {
     if(player){
         document.getElementById(id).style.backgroundColor="#0066ff";
-        // countColor++;
         player = !player
-        for (var i=0; i < Object.values(colorBlue).length; i++) {
-            if (Object.values(colorBlue)[i] == '') {
-                // colorBlue.Object.keys
-                var j = Object.keys(colorBlue)[i]
-                colorBlue[j] = id
-                console.log(j)
-                break;
-            }
-        }
+        colorBlue.push(id)
+        colorList.push(id)
+        console.log(colorBlue)
         gameOver();
     } else {
         document.getElementById(id).style.backgroundColor="#00cc00";
-        // countColor++;
         player = !player
-        for (var i=0; i < Object.values(colorGreen).length; i++) {
-            if (Object.values(colorGreen)[i] == '') {
-                var j = Object.keys(colorGreen)[i]
-                colorGreen[j] = id
-                console.log(j)
-                break;
-            }
-        }
+        colorGreen.push(id)
+        colorList.push(id)
+        console.log(colorGreen)
+        console.log(colorList)
         gameOver();
     }
 }
 
 //check the game is finished or not
 function gameOver() {
-    console.log(colorBlue)
+    // console.log(colorBlue)
 }
 
 
